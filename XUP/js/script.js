f8b639de80497e19f6923bb2829eb80333d0e831
vitaliy28073 <vikkttt@mail.ru>
2020-11-22 21:51:58 +0300

    commit

---
+++ XUP/js/script.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // function initAccordeon() {
+    //     let firstSectionBodyHeight = document.querySelector('.accordeon-section .accordeon-body > *').clientHeight
+    //     document.querySelector('.accordeon-section .accordeon-body').style.maxHeight = firstSectionBodyHeight + 'px'
+    // }
+
+    // initAccordeon()
+
+
+
+    let accordeonHeaderClickHandler = function (e) {
+        document.querySelectorAll('.js-section').forEach(function (section) {
+            section.querySelector('.js-content').style.maxHeight = '0px'
+        })
+
+        const accordeonSection = e.target.closest('.js-section')
+
+        const insideElHeight = accordeonSection.querySelector('.js-content > *').clientHeight
+
+        accordeonSection.querySelector('.js-content').style.maxHeight = insideElHeight + 'px'
+    }
+
+    document.querySelectorAll('.js-section')
+        .forEach(function (section) {
+            section.addEventListener('click', accordeonHeaderClickHandler)
+        })
+
+
+
+    const accordions = document.querySelectorAll('.js-section');
+
+    for (item of accordions) {
+        item.addEventListener('click', function () {
+
+            console.log(this);
+
+            if (this.classList.contains('active')) {
+
+                document.querySelectorAll('.js-section').forEach(function (section) {
+                    section.querySelector('.js-content').style.maxHeight = '0px'
+                })
+                this.classList.remove('active');
+            } else {
+                for (el of accordions) {
+                    el.classList.remove('active');
+                }
+                this.classList.add('active');
+            }
+        })
+    }
+
+})
